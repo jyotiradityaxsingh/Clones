141870c6f2aaa10cc7cc7b5d4e5b099241569f46
Jyotiraditya Singh <jyotiradityaxsinghchauhan@gmail.com>
2025-07-30 21:08:54 +0530

    One

--- One/src/App.jsx
+++ One/src/App.jsx
@@ -4,6 +4,7 @@ import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
 import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Workflow />
         <Pricing />
         <Testimonials />
+        <Footer />
       </div>
     </>
   );
