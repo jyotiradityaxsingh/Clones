a332a5320d0528bb010fdc834f728ba19008385e
Jyotiraditya Singh <jyotiradityaxsinghchauhan@gmail.com>
2025-08-08 21:23:10 +0530

    Three

---
+++ Three/src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>
+        Projects <span>Completed</span>
+        {/* 1:13 */}
+      </h1>
+    </div>
+  );
+}
+
+export default Projects;
